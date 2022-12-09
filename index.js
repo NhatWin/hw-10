7ac82d0ae061d5968947aa498f4cbc7ec74e9026
NhatWin <nhat0933@gmail.com>
2022-12-08 23:22:22 -0600

    Added question system to application

---
+++ index.js
@@ -0,0 +1,111 @@
+const inquirer = require("inquirer");
+const prompt = inquirer.createPromptModule();
+
+const leaderQuestions = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "What is the manager's name?",
+  },
+  {
+    type: "number",
+    name: "managerId",
+    message: "What is the manager's employ ID?",
+  },
+  {
+    type: "input",
+    name: "managerEmail",
+    message: "What is the manager's email?",
+  },
+  {
+    type: "number",
+    name: "managerNum",
+    message: "What is the manager's office number?",
+  },
+  {
+    type: "rawlist",
+    name: "teamMember",
+    message: "pick team role",
+    choices: ["engineer", "intern", "completed team"],
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "engineerName",
+    message: "What is the engineer's name?",
+  },
+  {
+    type: "number",
+    name: "engineerId",
+    message: "What is the engineer's employ ID?",
+  },
+  {
+    type: "input",
+    name: "engineerEmail",
+    message: "What is the engineer's email?",
+  },
+  {
+    type: "input",
+    name: "engineerGit",
+    message: "What is the engineer's GitHub?",
+  },
+  {
+    type: "rawlist",
+    name: "teamMember",
+    message: "pick team role",
+    choices: ["engineer", "intern", "completed team"],
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "internName",
+    message: "What is the intern's name?",
+  },
+  {
+    type: "number",
+    name: "internId",
+    message: "What is the intern's employ ID?",
+  },
+  {
+    type: "input",
+    name: "internEmail",
+    message: "What is the intern's email?",
+  },
+  {
+    type: "input",
+    name: "internSchool",
+    message: "What is the intern's school?",
+  },
+  {
+    type: "rawlist",
+    name: "teamMember",
+    message: "pick team role",
+    choices: ["engineer", "intern", "completed team"],
+  },
+];
+
+const createTeamLead = () => {
+  prompt(leaderQuestions).then((data) => {
+    createTeam(data);
+  });
+};
+
+const createTeam = (data) => {
+  if (data.teamMember === "engineer") {
+    prompt(engineerQuestions).then((data) => {
+      createTeam(data);
+    });
+  } else if (data.teamMember === "intern") {
+    prompt(internQuestions).then((data) => {
+      createTeam(data);
+    });
+  } else {
+    console.log("All done!");
+  }
+};
+
+createTeamLead();
