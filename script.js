793dbcf207fd404258cab19061d6c96558e15060
NhatWin <nhat0933@gmail.com>
2022-12-13 23:12:18 -0600

    Manager card made in html when file is created

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const Manager = require("./lib/Manager.js");
 const Intern = require("./lib/Intern.js");
 const Engineer = require("./lib/Engineer.js");
 
+let leadManager = {};
 const engineerTeam = [];
 const internTeam = [];
 
@@ -148,7 +149,7 @@ const internQuestions = [
 
 const createTeamLead = () => {
   prompt(leaderQuestions).then((manager) => {
-    createLeadCard(manager);
+    saveLeader(manager);
     createTeam(manager);
   });
 };
@@ -156,29 +157,31 @@ const createTeamLead = () => {
 const createTeam = (data) => {
   if (data.teamMember === "engineer") {
     prompt(engineerQuestions).then((data) => {
+      saveTeam(data);
       creatTeamCard(data);
-      createTeam(data);
     });
   } else if (data.teamMember === "intern") {
     prompt(internQuestions).then((data) => {
-      createTeam(data);
+      saveTeam(data);
       creatTeamCard(data);
     });
   } else {
+    writeToFile("team.html");
     console.log("All done!");
   }
 };
 
-const createLeadCard = (manager) => {
+const saveLeader = (manager) => {
   const lead = new Manager(
     manager.name,
     manager.id,
     manager.email,
     manager.num
   );
+  leadManager = lead;
 };
 
-const creatTeamCard = (team) => {
+const saveTeam = (team) => {
   if (team.school === undefined) {
     const engineer = new Engineer(team.name, team.id, team.email, team.git);
     engineerTeam.push(engineer);
@@ -188,12 +191,83 @@ const creatTeamCard = (team) => {
   }
 };
 
+const createLeadCard = (data) => {
+  return `<div class="team">
+  <h2>
+    <img src="./Assets/manager.png" alt="manager icon" width="40" />${data.name}
+  </h2>
+  <p>ID: ${data.id}</p>
+  <p>Email: ${data.email}</p>
+  <p>Office #: ${data.num}</p>
+</div>`;
+};
+
 createTeamLead();
 
-// function writeToFile(fileName, data) {
-//   generateMarkdown;
-//   const info = generateMarkdown(data);
-//   fs.writeFile(fileName, ``, (err) =>
-//     err ? console.error(err) : console.log("Success!")
-//   );
-// }
+function writeToFile(fileName) {
+  fs.writeFile(
+    fileName,
+    `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <!-- CSS only -->
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+      <link
+        href="https://fonts.googleapis.com/css2?family=Martian+Mono:wght@600&family=Rubik:wght@300&display=swap"
+        rel="stylesheet"
+      />
+      <link
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+        rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65"
+        crossorigin="anonymous"
+      />
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/modern-normalize@1.1.0/modern-normalize.css"
+      />
+      <link rel="stylesheet" href="./src/style.css" />
+      <title>HTML-CSS Practice</title>
+    </head>
+    <body>
+      <header class="bg-warning text-dark p-5">
+        <h1 class="text-center text-white">My Team</h1>
+      </header>
+      <section id="leader">
+        ${createLeadCard(leadManager)}
+      </section>
+      <hr />
+      <main>
+        <div class="team">
+          <h2>
+            <img src="./Assets/engineer.png" alt="manager icon" width="40" />Nhat
+          </h2>
+          <p>ID: 2</p>
+          <p>Email: nhat0933@gmail.com</p>
+          <p>GitHub: NhatWin</p>
+        </div>
+        <div class="team">
+          <h2>
+            <img src="./Assets/intern.png" alt="manager icon" width="40" />Nhat
+          </h2>
+          <p>ID: 2</p>
+          <p>Email: nhat0933@gmail.com</p>
+          <p>School: WashU</p>
+        </div>
+      </main>
+      <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4"
+        crossorigin="anonymous"
+      ></script>
+    </body>
+  </html>
+  `,
+    (err) => (err ? console.error(err) : console.log("Success!"))
+  );
+}
